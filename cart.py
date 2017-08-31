b3642895a3315d5eb86cecfe4c99336f543890b8
Cal <callen-johnson@utulsa.edu>
2017-08-31 18:27:59 -0500

    Add function to calculate the accuracy percentage

--- cart.py
+++ cart.py
@@ -38,6 +38,16 @@ def cross_validation_split(dataset, n_folds):
         dataset_split.append(fold)
     return dataset_split
 
+# calculate accuracy percentage
+def accuracy_metric(actual, predicted):
+    correct = 0
+    for i in range(len(actual)):
+        if actual[i] == predicted[i]:
+            correct += 1
+        return correct / float(len(actual)) * 100.0
+
+
+
 '''
 This section will be to test then determine the splits in a given 
 dataset. Then the Gini index will be calculated.
