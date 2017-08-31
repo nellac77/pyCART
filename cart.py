7b22fd1268cbb88098f9ff773d77458ab922b166
Cal <callen-johnson@utulsa.edu>
2017-08-31 18:35:26 -0500

    Add function to evaluate an algorithm with cross validation split

--- cart.py
+++ cart.py
@@ -46,7 +46,27 @@ def accuracy_metric(actual, predicted):
             correct += 1
         return correct / float(len(actual)) * 100.0
 
-
+# evaluate an algorithm using a cross validation split
+def evaluate_algorithm(dataset, algorithm, n_folds, *args):
+    folds = cross_validation_split(dataset, n_folds)
+    scores = list()
+    for fold in folds:
+        train_set = list(folds)
+        train_set.remove(fold)
+        train_set = sum(train_set, [])
+        test_set = list()
+        for row in fold:
+            row_copy = list(row)
+            test_set.append(row_copy)
+            row_copy[-1] = None
+        predicted = algorithm(train_set, test_set, *args)
+        actual = [row[-1] for row in fold]
+        accuarcay = accuracy_metric(actual, predicted)
+        scores.append(accuarcay)
+    return scores
+        
+        
+        
 
 '''
 This section will be to test then determine the splits in a given 
