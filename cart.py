f00699ebe3695254b0e12807eda6aec8705042c0
Cal <callen-johnson@utulsa.edu>
2017-08-31 18:24:44 -0500

    Add function to split dataset into k integer folds

--- cart.py
+++ cart.py
@@ -24,6 +24,19 @@ def load_csv(filename):
 def str_column_to_float(dataset, column):
     for row in dataset:
         row[column] = float(row[column].strip())
+        
+# split the dataset into k folds
+def cross_validation_split(dataset, n_folds):
+    dataset_split = list()
+    dataset_copy = list(dataset)
+    fold_size = int(len(dataset) / n_folds)
+    for i in range(n_folds):
+        fold = list()
+        while len(fold) < fold_size:
+            index = randrange(len(dataset_copy))
+            fold.append(dataset_copy.pop(index))
+        dataset_split.append(fold)
+    return dataset_split
 
 '''
 This section will be to test then determine the splits in a given 
