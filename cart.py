ce5338c102b9487c67eae9bff3c20c9301af26b0
Cal <callen-johnson@utulsa.edu>
2017-08-31 13:45:40 -0500

    Add functions to choose best split point in a dataset and split the dataset based on attribute and the value of the attribute

--- cart.py
+++ cart.py
@@ -29,3 +29,27 @@ def gini_index(groups, classes):
 #print (gini_index([[[1, 1], [1, 0]], [[1, 1], [1, 0]]], [0, 1]))
 # Best case, 0.0
 #print (gini_index([[[1, 0], [1, 0]], [[1, 1], [1, 1]]], [0, 1]))
+
+# Split dataset based on an attribute and its value
+def test_split(index, value, dataset):
+    left, right = list(), list()
+    for row in dataset:
+        if row[index] < value:
+            left.append(row)
+        else:
+            right.append(row)
+    # note: right contains all rows with value at index
+    # greater than or equal to split value
+    return left, right
+
+# Choose best split point for the dataset via exhaustive, greedy algorithm
+def get_split(dataset):
+    class_values = list(set(row[-1] for row in dataset))
+    b_index, b_value, b_score, b_groups = 999, 999, 999, None
+    for index in range(len(dataset[0])-1):
+        for row in dataset:
+            groups = test_split((index), row[index], dataset)
+            gini = gini_index(groups, class_values)
+            if gini < b_score:
+                b_index, b_value, b_score, b_groups = index, row[index], gini, groups
+    return {'index ':b_index, 'value ':b_value, 'score ':b_score, 'groups ':b_groups}
