c9a3fa911be6068045cf50942e372205e8e4db8c
Cal <callen-johnson@utulsa.edu>
2017-08-31 23:59:33 -0500

    Fix issue with accuracy_metric function, code works on real-world data set

--- cart.py
+++ cart.py
@@ -40,11 +40,11 @@ def cross_validation_split(dataset, n_folds):
 
 # calculate accuracy percentage
 def accuracy_metric(actual, predicted):
-    correct = 0
-    for i in range(len(actual)):
-        if actual[i] == predicted[i]:
-            correct += 1
-        return correct / float(len(actual)) * 100.0
+	correct = 0
+	for i in range(len(actual)):
+		if actual[i] == predicted[i]:
+			correct += 1
+	return correct / float(len(actual)) * 100.0
 
 # evaluate an algorithm using a cross validation split
 def evaluate_algorithm(dataset, algorithm, n_folds, *args):
